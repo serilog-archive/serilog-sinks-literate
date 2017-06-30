79705d1f71ad03c60dea78cf495e28c31da5b9a1
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-06-30 10:04:54 +1000

    Fixes #19 - wrap the new Serilog.Sinks.Console

--- src/Serilog.Sinks.Literate/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Serilog.Sinks.Literate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2015")]
